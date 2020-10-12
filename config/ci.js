b5e8fbef4ef2044e1443ca46e0c5cc429ff1445c
cojosh90 <cojosh90@gmail.com>
2020-10-12 14:07:43 +0800

    travis config

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
     googleClientID:
       '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
     googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-    mongoURI: '127.0.0.1:27017/blog_ci',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
     redisUrl: 'redis://127.0.0.1:6379',
     cookieKey: '123123123',
   };
